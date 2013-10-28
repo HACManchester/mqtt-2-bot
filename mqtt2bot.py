29e49bcfc700562c6b7dee80f33052e13864dee6
Bob Clough <bob@clough.me>
2013-10-28 15:20:21 +0000

    Added last seen functionality to the bot

--- mqtt2bot.py
+++ mqtt2bot.py
@@ -2,6 +2,9 @@
 import mosquitto
 import random 
 import socket
+import sqlite3
+import time
+from prettydate import pretty_date
 
 def send_to_bot(message, room="#hacman"):
     try:
@@ -12,15 +15,38 @@ def send_to_bot(message, room="#hacman"):
     except Exception, e:
         pass
 
+def last_seen(username):
+	conn = sqlite3.connect('last_seen.db')
+	c = conn.cursor()
+	un = (username,)
+	c.execute("SELECT timestamp FROM lastseen WHERE username=?", un)
+	ls = c.fetchone()
+	c.execute("DELETE FROM lastseen WHERE username=?;", un)
+        c.execute("INSERT INTO lastseen VALUES(?,?);", (username, int(time.time())))
+	conn.commit()
+	conn.close()
+	return ls
+
+
 def on_message(mosq, obj, msg):
 	if msg.topic == 'door/outer/open/username':
-		send_to_bot("%s opened the outer door" % msg.payload)
+		ls = last_seen(msg.payload)
+		if not ls:
+			send_to_bot("%s opened the outer door." % msg.payload)
+		else:
+                        send_to_bot("%s opened the outer door. Last seen %s." % (msg.payload, pretty_date(ls[0])))
 	elif msg.topic == 'door/outer/buzzer':
 		send_to_bot(random.choice(['Buzzer', 'Buzzer', 'Buzzer', 'rezzuB', 'Buzzard']))
 	elif msg.topic == 'door/outer/invalidcard':
 		send_to_bot("Unknown card at door")
 	elif msg.topic == 'bot/outgoing':
 		send_to_bot(msg.payload)
+
+conn = sqlite3.connect('last_seen.db')
+c = conn.cursor()
+c.execute("CREATE TABLE IF NOT EXISTS lastseen (username TEXT, timestamp INTEGER);")
+conn.commit()
+conn.close()
 	
 mqttc = mosquitto.Mosquitto("mqtt2bot")
 mqttc.connect("alfred")
